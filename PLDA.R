af95687b6b7393ac7f1bafc1986e97978edb5b7d
Johanna Vikkula <johannavikkula@Johanna-MacBook-Air.local>
2019-03-05 12:17:21 +0200

    Implement computation of shat for differnet methods

--- PLDA.R
+++ PLDA.R
@@ -1,4 +1,4 @@
-## Poisson Linear Discriminant Anallysis classifier
+# Poisson Linear Discriminant Analysis classifier
 # This implementation is largely based on the PoiClaClu package
 # Own implementation for the purpose of understanding it better
 
@@ -9,11 +9,13 @@ PLDA <- function(x,x_test, y, beta = 1, prior=NULL, method = c('mle','deseq','qu
   # beta : prior for Gamma(beta, beta) distribution
   # prior : prior probabilities for classes y. Vector of lenght unique(y)
   # method: method to compute size factor
+  # return predicted classes and the log probabbilities for each observation
   
   Xi_dot = rowSums(x)
   Xdot_j = colSums(x)
   Xdotdot = sum(x)
   
+  # Implement method which takes into account the method (mle, deseq or quantile), when computing Nhat
   Nhat = outer(Xi_dot, Xdot_j, '*')/Xdotdot
   
   classes = sort(unique(y))
@@ -32,22 +34,34 @@ PLDA <- function(x,x_test, y, beta = 1, prior=NULL, method = c('mle','deseq','qu
   
   p = matrix(0,ncol = length(classes),nrow = nrow(x_test))
   for (i in 1:length(classes)){
-    p[,i] = rowSums(x_test*1/log(dhat[i,]))-rowSums(outer(shat, ghat, '*')*1/dhat[i,])+log(prior[i])
-    #p[,i] = exp(log_p) 
-    #p[,i] = rowSums(scale(x_test, center=FALSE, scale=(1/log(dhat[i,])))) - rowSums(scale(outer(shat, ghat, '*'), center = FALSE, scale = (1/dhat[i,]))) + log(prior[i])
+    #p[,i] = rowSums(x_test*log(dhat[i,]))-rowSums(outer(shat, ghat, '*')*dhat[i,])+log(prior[i])
+    #p[,i] = rowSums(x_test/log(dhat[i,]))-rowSums(outer(shat, ghat, '*')/dhat[i,])+log(prior[i])
+    p[,i] = rowSums(scale(x_test, center=FALSE, scale=(1/log(dhat[i,])))) - rowSums(scale(outer(shat, ghat, '*'), center = FALSE, scale = (1/dhat[i,]))) + log(prior[i])
   }
   yhat = classes[apply(p,1,which.max)]
-  return(yhat) #maybe good to return something else too
+  return(list(p = p, yhat = yhat)) 
 }
 
 
 compute_shat <- function(x_train,x_test,method = c('mle','deseq','quatile')){
-  # NOTE! For now only mle possible! Implementation for deseq and quantile is in progress
   # Compute size factor based on defined method
-  # 'mle'
-  # 'deseq'
-  # 'quantile'
- if (method == 'mle')
-   shat <- rowSums(x_test)/sum(x_train)
- return(shat)
+  # 'mle' : total count for the ith observation, which is based on the MLE for Nij 
+  # 'deseq' : median ratio
+  # 'quantile' 
+  
+  if (method == 'mle'){
+    shat = rowSums(x_test)/sum(x_train)
+  }
+  else if (method == 'quantile'){
+    shat = pmax(1, apply(x_test, 1, quantile, 0.75))
+    shat = shat/sum(apply(x_train, 1, quantile, 0.75))
+  }  
+  else if (method == 'deseq'){
+    counts_train <- t(x_train)
+    counts_test <- t(x_test)
+    geomeans <- exp(rowMeans(log(counts_train)))
+    rawsizestr <- apply(counts_train, 2, function(cnts) median((cnts/geomeans)[geomeans > 0]))
+    shat <- apply(counts_test, 2, function(cnts) median((cnts/geomeans)[geomeans > 0], na.rm = TRUE))/sum(rawsizestr)
+  }
+  return(shat)
 }
